649ec024a3a7399f3a32de83865b0c840b094fd3
benwig <gwen.simswilliams@gmail.com>
2018-04-23 13:45:28 +0100

    Set things up for handlebars

---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+/*----------------------------------------------------------------------
+Here's where we pull all the libraries together and render the JS elements of the page.
+Whole thing is wrapped in an anonymous self-executing function to avoid polluting global namespace.
+-----------------------------------------------------------------------*/
+
+/*jshint esversion:6, devel: true, browser: true*/
+
+(function () {
+	
+	/********************/
+  /* SET UP VARIABLES */
+  /********************/
+	
+	const studentId = 'student1';
+	//we'll set up handlbars templates here too
+	
+	/************************/
+  /* BIND EVENT LISTENERS */
+  /************************/
+	
+	document.addEventListener('DOMContentLoaded', () => {
+		
+		//we'll run nautilus.init in here, with callback which does the handlebars work and edits the DOM.
+		
+	});
+	
+	/********************/
+  /* HELPER FUNCTIONS */
+  /********************/
+	
+	//shortcut which generates & returns a handlebars template
+	function getTemplate (elementId) {
+    return Handlebars.compile(document.getElementById(elementId).innerHTML);
+  }
+
+	
+})();
