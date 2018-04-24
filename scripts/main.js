13eb4308f81385761c5278c3eccf3cb8eb8ebbec
benwig <gwen.simswilliams@gmail.com>
2018-04-24 17:20:11 +0100

    Render unit table with nautilus data

--- scripts/main.js
+++ scripts/main.js
@@ -13,41 +13,72 @@ Whole thing is wrapped in an anonymous self-executing function to avoid pollutin
 	
 	const studentId = 'student1',
 				unitSummaryTemplate = getTemplate('unitSummaryTemplate'),
-				charts = {}; //holds variables created by chartist.js
+				tableRowTemplate = getTemplate('tableRowTemplate'),
+				charts = {}, //holds variables created by chartist.js
+				myScores = document.getElementById('myScores');
 	
+	let unitTable;
+
 	/************************/
   /* BIND EVENT LISTENERS */
   /************************/
 	
 	document.addEventListener('DOMContentLoaded', () => {
 		nautilus.init( () => {
-			//first we get the names of units, and derive number of units,
-			//and we set up variables which we'll need in our for loop.
-			const unitNames = nautilus.getUnitNames(),
-		  			len = unitNames.length;
-			let i = 0;
-      
-			//first loop. For each unit, add a div to the page.
-			for (i; i < len; i++) {
-				//start building our context object using the current unit's summary data
-        let context = nautilus.getUnitSummary('student1', unitNames[i]);
-				//add the unit name to the context object
-				context.unitName = unitNames[i];
-				context.num = i;
-				//compile Handlebars html and place it into the DOM
-				document.getElementById('myScores').innerHTML += unitSummaryTemplate(context);
-			}
-			//second loop because chartist doesn't want to behave in loop 1. Add charts to each div.
-			for (i = 0; i < len; i++) {
-        let summary = nautilus.getUnitSummary('student1', unitNames[i]);
-				makeChart(`#unitchart-${i}`, summary.degreeValues, summary.percentCompleted);
-			}
+			renderUnitSummaries();
+			//add a click listener to each div inside myScores
+			for (let i = 0; i < myScores.childNodes.length; i++) {
+				myScores.childNodes[i].addEventListener('click', (e) => {
+					renderUnitTable(e);
+				});
+			};
 		});
-		
-		//apply configurations to the data table
-		configureDataTable('unitTable');
-		
+		unitTable = configureDataTable('unitTable');
 	});
+	
+	/*********************************/
+  /* FUNCTIONS FOR EVENT LISTENERS */
+  /*********************************/
+	
+	function renderUnitSummaries () {
+		//first we get the names of units, and derive number of units,
+		//and we set up variables which we'll need in our for loop.
+		const unitNames = nautilus.getUnitNames(),
+					len = unitNames.length;
+		let i = 0;
+
+		//first loop. For each unit, add a div to the page.
+		for (i; i < len; i++) {
+			//start building our context object using the current unit's summary data
+			let context = nautilus.getUnitSummary('student1', unitNames[i]);
+			//add the unit name to the context object
+			context.unitName = unitNames[i];
+			context.num = i;
+			//compile Handlebars html and place it into the DOM
+			document.getElementById('myScores').innerHTML += unitSummaryTemplate(context);
+		}
+		//second loop because chartist doesn't want to behave in loop 1. Add charts to each div.
+		for (i = 0; i < len; i++) {
+			let summary = nautilus.getUnitSummary('student1', unitNames[i]);
+			makeChart(`#unitchart-${i}`, summary.degreeValues, summary.percentCompleted);
+		}
+	}
+	
+	function renderUnitTable (event) {
+		const unitName = event.currentTarget.dataset.unit,
+					results = nautilus.getUnitResults(studentId, unitName),
+					tableRows = [];
+		let i = 0;
+		for (i; i < results.length; i++) {
+			tableRows.push([
+				results[i].LO_name,
+				results[i].first_score,
+				results[i].best_score,
+				results[i].attempts
+			]);
+		}
+		unitTable.clear().rows.add(tableRows).draw();
+	}
 	
 	/********************/
   /* HELPER FUNCTIONS */
@@ -59,7 +90,7 @@ Whole thing is wrapped in an anonymous self-executing function to avoid pollutin
   }
 	
 	function configureDataTable (elementId) {
-		$(`#${elementId}`).DataTable({
+		return $(`#${elementId}`).DataTable({
 			"paging":   false,
        "info":     false
 		});
